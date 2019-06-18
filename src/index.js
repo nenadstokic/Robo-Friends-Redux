677f37e2c7fbf31d72ac7c3a4c9f09868534a172
Nenad <nstokic@gmail.com>
2019-06-18 19:44:10 +0200

    redux nastavak ali ne radi još

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// provider komponenta prosleđuje store celoj aplikaciji
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots});
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+                <Provider store={store}>
+                  <App />
+                </Provider>, document.getElementById('root')
+                );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,44 @@
-const initialState = {
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from './constants'
+
+const initialStateSearch = {
   searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
+  // react documentation recommends switch, we can use 'if'
   switch(action.type) { // type iz actions fajla
     case CHANGE_SEARCH_FIELD:
+        /* we receive an action called CHANGE_SEARCH_FIELD than return the new state
+        and return new object that has everything in the state plus we're gonna update
+        the search property with the new value the user has entered */
       return Object.assign({}, state, {searchField: action.payload});
+        // this can be written this way too:
+        // return { ...state, searchField: action.payload }
+
+    default:
+      return state;
+  }
+}
+
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+  switch(action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true })
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, { robots: action.payload, isPending: false })
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, { error: action.payload, isPending: false})
     default:
       return state;
   }
